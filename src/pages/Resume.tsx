14334e5d11f4f5a87407a95fb98ad870f0323841
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-29 17:45:28 -0400

    update: animations

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -2,7 +2,7 @@ import { motion as m } from 'framer-motion';
 
 export default function Resume() {
   return (
-    <m.div className="resume" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.75, ease: 'easeOut' }}>
+    <m.div className="resume" initial={{ opacity: 0 }} animate={{ opacity: 1, rotate: -360 }} transition={{ duration: 0.75, ease: 'easeOut' }}>
         <a className='download-button' href="/CV.pdf" download>Download</a>
     </m.div>
   );

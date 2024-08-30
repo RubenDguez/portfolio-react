674c61872b8bda908d30f2e12ea191dd1563aa77
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-30 14:35:06 -0400

    update: resume download button

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -2,8 +2,11 @@ import { motion as m } from 'framer-motion';
 
 export default function Resume() {
   return (
-    <m.div className="resume" initial={{ opacity: 0 }} animate={{ opacity: 1, rotate: -360 }} transition={{ duration: 0.75, ease: 'easeOut' }}>
-        <a className='download-button' href="/CV.pdf" download>Download</a>
+    <m.div className="resume" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.75 }}>
+        <m.a whileHover={{ scale: 1.1 }} className='download-button' href="/CV.pdf" download>
+          <div id='download-image'></div>
+          <div id='download-text'>Download</div>
+        </m.a>
     </m.div>
   );
 }

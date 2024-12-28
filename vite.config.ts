d53acb45da9e5e15c8bbba5c9a11dc5335ceb59f
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-28 14:36:12 -0500

    update: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://rubendguez.github.io/portfolio-react/",
   plugins: [react()],
   server: {
     port: 3000,

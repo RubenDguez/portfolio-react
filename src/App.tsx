8fb061001de312f24005ff7bf0d8b414858bac54
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-30 19:30:22 -0500

    switch to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import { AnimatePresence } from 'framer-motion';
-import { Outlet } from 'react-router-dom';
+import {AnimatePresence} from 'framer-motion';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 
 import Footer from './components/Footer';
 import Header from './components/Header';
 
+import React from "react";
+import Portfolio from "./pages/Portfolio.tsx";
+import Contact from "./pages/Contact.tsx";
+import Resume from "./pages/Resume.tsx";
+import About from "./pages/About.tsx";
+
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <div className='outlet'>
-        <AnimatePresence>
-          <Outlet />
-        </AnimatePresence>
-      </div>
-      <Footer />
-    </div>
+    <HashRouter>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <div className='app'>
+          <Header/>
+          <div className='outlet'>
+            <AnimatePresence>
+              <Routes>
+                <Route path="/" element={<About/>}/>
+                <Route path="/portfolio" element={<Portfolio/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/resume" element={<Resume/>}/>
+              </Routes>
+            </AnimatePresence>
+          </div>
+          <Footer/>
+        </div>
+      </React.Suspense>
+    </HashRouter>
   );
 }
+
 
 export default App;
